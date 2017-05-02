12dc7c328b51318000376f110b297bf0bb17de4e
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-05-02 18:57:23 +0800

    add image in new and edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
-  
+
   def index
     @products = Product.all
   end
@@ -45,7 +45,7 @@ class Admin::ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:title, :description, :price, :quantity,)
+    params.require(:product).permit(:title, :description, :price, :quantity, :image,)
   end
 
 
